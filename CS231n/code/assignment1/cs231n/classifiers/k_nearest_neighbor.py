67c3aa8337cb3794307e15d9699e10ba1517229a
liuzengyun_ubuntu <i@cvzooo.cn>
2022-08-29 15:56:27 +0800

    ubuntu829

--- CS231n/code/assignment1/cs231n/classifiers/k_nearest_neighbor.py
+++ CS231n/code/assignment1/cs231n/classifiers/k_nearest_neighbor.py
@@ -73,7 +73,7 @@ class KNearestNeighbor(object):
         # training point, and store the result in dists[i, j]. You should   #
         # not use a loop over dimension.                                    #
         #####################################################################
-        pass
+        dists[i,j]=np.sqrt(np.sum(np.square(X[i,:]-self.X_train[j,:])))
         #####################################################################
         #                       END OF YOUR CODE                            #
         #####################################################################
--- Pytorch_tudui/code/P21_正则化_其他层.py
+++ Pytorch_tudui/code/P21_正则化_其他层.py
@@ -4,6 +4,37 @@
 # Email:i@cvzoo.cn
 
 
+import torchvision
+import torch
+from torch.utils.data import DataLoader
+
+data_set = torchvision.datasets.CIFAR10("./dataset", train=False, transform=torchvision.transforms.ToTensor(),
+                                        download=True)
+dataloader = DataLoader(data_set,batch_size=64)
+
+class Tudui(torch.nn.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.linear1 = torch.nn.Linear(196608, 10)
+
+    def forward(self, input):
+        output = self.linear1(input)
+        return output
+
+
+
+tudui = Tudui()
+for data in dataloader:
+    imgs, tars = data
+    print(imgs.shape)
+    output = torch.flatten(imgs)# 展平，展成一行数据
+    print(output.shape)
+    output = tudui(output)
+    print(output.shape)
+
+# 报错是因为最后一个batch的数据展开不够196608
+
 
 
 
